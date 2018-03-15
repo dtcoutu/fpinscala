689c14d4bcfb9dfa823e23043d9cb710e775911a
David Coutu <dcoutu@inntopia.com>
2018-03-14 23:06:32 -0400

    Exercise 3.3

--- exercises/src/test/scala/datastructures/ListTest.scala
+++ exercises/src/test/scala/datastructures/ListTest.scala
@@ -8,4 +8,9 @@ class ListTest extends FlatSpec with Matchers {
 		fpinscala.datastructures.List.tail(Nil) shouldBe Nil
 		fpinscala.datastructures.List.tail(List("a", "z", "y")) shouldBe List("z", "y")
 	}
+
+	"setHead" should "add element to beginning of given list" in {
+		fpinscala.datastructures.List.setHead(List(2,3), 1) shouldBe List(1,2,3)
+		fpinscala.datastructures.List.setHead(Nil, 1) shouldBe List(1)
+	}
 }
