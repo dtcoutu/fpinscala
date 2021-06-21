f0b54afad1920286a6f8ca9fe67c8c9c38a201f0
David Coutu <dcoutu@covermymeds.com>
2021-06-20 22:45:16 -0400

    Implement 3.11 with some updates

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -39,7 +41,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x:Int = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -67,51 +69,62 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
-  def sum2(ns: List[Int]) =
+  def sum2(ns: List[Int]): Int =
     foldRight(ns, 0)((x,y) => x + y)
 
   /* Exercise 3.7
    * Can product, implemented using foldRight, immediately halt the recursion and return 0.0
    * if it encounters a 0.0?  Why or why not?
    *
-   * foldRight would need to know what function it is being applied to know that it should short
+   * No, foldRight would need to know what function it is being applied to in order to know that it could short
    * circuit the logic.
    */
-  def product2(ns: List[Double]) =
+  def product2(ns: List[Double]): Double =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
   // Exercise 3.2
   def tail[A](l: List[A]): List[A] = l match {
     case Nil => Nil
-    case Cons(x, xs) => xs
+    case Cons(_, t) => t
   }
 
-  def setHead[A](l: List[A], h: A): List[A] = Cons(h, l)
+  // Exercise 3.3
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => Cons(h, t)
+  }
 
+  // Exercise 3.4
+  @tailrec
   def drop[A](l: List[A], n: Int): List[A] = {
     if (n <= 0) l
     else l match {
       case Nil => Nil
-      case Cons(_, ls) => drop(ls, n-1)
+      case Cons(_, t) => drop(t, n-1)
     }
   }
 
+  // Exercise 3.5
+  @tailrec
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Nil => Nil
-    case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
+    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
   }
 
+  // Exercise 3.6
+  // This can't be constant time like tail because it must walk through the entire list to reach what it must drop.
   def init[A](l: List[A]): List[A] = l match {
       case Nil => Nil
       case Cons(_, Nil) => Nil
-      case Cons(x, xs) => Cons(x, init(xs))
+      case Cons(h, t) => Cons(h, init(t))
     }
 
   // Exercise 3.9
-  def length[A](l: List[A]): Int = foldRight(l, 0)((a,b) => b+1)
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_,acc) => acc+1)
 
   // Exercise 3.10
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    @tailrec
     def go(remaining: List[A], acc: B): B = remaining match {
       case Nil => acc
       case Cons(x, xs) => go(xs, f(acc, x))
@@ -119,6 +132,18 @@ object List { // `List` companion object. Contains functions for creating and wo
 
     go(l, z)
   }
+
+  // Exercise 3.11
+  // Still need to get used to more generic anonymous functions...
+  // ((acc, a) => acc + a) ==> (_ + _)
+  def sum3(ns: List[Int]): Int =
+    foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]): Double =
+    foldLeft(ns, 1.0)(_ * _)
+
+  def length3[A](l: List[A]): Int =
+    foldLeft(l, 0)((acc, _) => acc+1)
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
